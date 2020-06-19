38aeca335664d94c9935bee6eb0ececacd2a00af
T. Sunil Rao <t_sunil_rao@yahoo.com>
2020-06-19 13:26:56 +0530

    mat-ex 1.2.3

--- projects/mat-ex/src/public-api.ts
+++ projects/mat-ex/src/public-api.ts
@@ -5,13 +5,6 @@
 export * from './lib/components/toolbar-button/toolbar-button.module';
 export * from './lib/components/toolbar-button/toolbar-button.component';
 
-export * from './lib/components/dialog/dialog.module';
-export * from './lib/components/dialog/dialog/dialog.component';
-export * from './lib/components/dialog/dialog-title/dialog-title.component';
-export * from './lib/components/dialog/dialog-icon/dialog-icon.component';
-export * from './lib/components/dialog/dialog-content/dialog-content.component';
-export * from './lib/components/dialog/dialog-actions/dialog-actions.component';
-
 export * from './lib/mtx-profile/mtx-profile.module';
 export * from './lib/mtx-profile/mtx-profile/mtx-profile.component';
 export * from './lib/mtx-profile/mtx-profile-header/mtx-profile-header.component';
