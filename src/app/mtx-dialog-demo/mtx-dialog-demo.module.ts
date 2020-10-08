cde6a693181a7f2904678b7cca064baaf433caa6
T. Sunil Rao <t_sunil_rao@yahoo.com>
2020-10-08 19:39:30 +0530

    mat-ex-1.2.23

--- src/app/mtx-dialog-demo/mtx-dialog-demo.module.ts
+++ src/app/mtx-dialog-demo/mtx-dialog-demo.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MtxDialogDemoRoutingModule } from './mtx-dialog-demo-routing.module';
 import { MtxDialogDemoComponent } from './mtx-dialog-demo/mtx-dialog-demo.component';
-import { MtxAppBarModule, MtxDialogModule, MtxPageModule } from 'projects/mat-ex/src/public-api';
+import { MtxAppBarModule, MtxCardModule, MtxContainerModule, MtxDialogModule, MtxPageModule } from 'projects/mat-ex/src/public-api';
 import { FooterSectionModule } from '../shared/footer-section/footer-section.module';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [MtxDialogDemoComponent],
   imports: [
-    CommonModule, FooterSectionModule, MatButtonModule,
-    MtxDialogDemoRoutingModule, MtxPageModule, MtxAppBarModule, MtxDialogModule
+    CommonModule, FooterSectionModule, MatButtonModule, MatIconModule, MatTooltipModule,
+    MtxDialogDemoRoutingModule, MtxPageModule, MtxAppBarModule, MtxDialogModule, MtxCardModule, MtxContainerModule
   ]
 })
 export class MtxDialogDemoModule { }
--- src/app/mtx-dialog-demo/mtx-dialog-demo/mtx-dialog-demo.component.ts
+++ src/app/mtx-dialog-demo/mtx-dialog-demo/mtx-dialog-demo.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MtxDialogDemoComponent implements OnInit {
 
+  dialog = 'basic';
+
   constructor() { }
 
   ngOnInit(): void {
