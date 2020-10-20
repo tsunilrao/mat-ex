73780a3bf125aa25db7910ee3280a4bb9e645a70
T. Sunil Rao <t_sunil_rao@yahoo.com>
2020-10-20 20:11:30 +0530

    mat-ex-1.2.25

--- projects/mat-ex/src/lib/mtx-container/mtx-container.component.ts
+++ projects/mat-ex/src/lib/mtx-container/mtx-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: `mtx-container, mtx-row, mtx-column`,
@@ -8,10 +8,13 @@ import { Component, OnInit, ViewEncapsulation, ElementRef } from '@angular/core'
     class: 'mtx-container', 
     '[class.mtx-row]': "selector=='mtx-row'",
     '[class.mtx-column]': "selector=='mtx-column'",
+    '[class.mtx-container-fill]': "fill!==undefined",
   },
   encapsulation: ViewEncapsulation.None,
 })
 export class MtxContainerComponent implements OnInit {
+
+  @Input() fill;
 
   get selector(): string { return this.hostRef.nativeElement.tagName.toLowerCase() }
 
