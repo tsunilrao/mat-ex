b3f051ed6f4b5f3fdc09cc9511c0b04c676b6fa8
T. Sunil Rao <t_sunil_rao@yahoo.com>
2020-12-12 03:19:44 +0530

    mtx-text-demo

--- src/app/mtx-text-demo/mtx-text-demo.component.ts
+++ src/app/mtx-text-demo/mtx-text-demo.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MtxTextDemoComponent implements OnInit {
 
+  public attributes = [
+    {attribute:'mtx-display', description:'Large, one-off header or a hero header).'}, 
+    {attribute:'mtx-headline', description:'Useful as section headings.'}, 
+    {attribute:'mtx-title', description:'Can be used in toolbars, cards etc.'}, 
+    {attribute:'mtx-subtitle', description:'Can be used instead of Titles for lower emphasis.'}, 
+    {attribute:'mtx-label', description:'Bold text generally used in buttons.'}, 
+    {attribute:'mtx-paragraph', description:'Regular paragraph text.'}, 
+    {attribute:'mtx-caption', description:'Smaller body and hint text.'}, 
+    {attribute:'mtx-overline', description:'Smallest text generally used as overlines.'}, 
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
