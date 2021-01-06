b210e32b86892343e68874b84af8c6c36a134818
tsunilrao <t_sunil_rao@yahoo.com>
2021-01-06 18:20:20 +0530

    mtx-1.3.6

--- projects/mat-ex/src/lib/mtx-nav/mtx-nav.component.ts
+++ projects/mat-ex/src/lib/mtx-nav/mtx-nav.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'mtx-nav',
   templateUrl: './mtx-nav.component.html',
-  styleUrls: ['./mtx-nav.component.scss']
+  styleUrls: ['./mtx-nav.component.scss'],
+  host: {
+    class: 'mtx-nav',
+    '[class.mtx-nav-item-active]': "active===true || active!==undefined",
+  },
+  encapsulation: ViewEncapsulation.None
 })
 export class MtxNavComponent implements OnInit {
+
+  @Input() active;
 
   constructor() { }
 
--- src/app/mtx-color-demo/mtx-color-demo.module.ts
+++ src/app/mtx-color-demo/mtx-color-demo.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { MtxColorDemoRoutingModule } from './mtx-color-demo-routing.module';
 import { MtxColorDemoComponent } from './mtx-color-demo.component';
+import { MtxAppBarModule, MtxContainerModule, MtxPageModule, MtxTextModule, MtxToolbarModule } from 'projects/mat-ex/src/public-api';
+import { FooterSectionModule } from '../shared/footer-section/footer-section.module';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 @NgModule({
   declarations: [MtxColorDemoComponent],
   imports: [
     CommonModule,
-    MtxColorDemoRoutingModule
+    MtxColorDemoRoutingModule,
+    MtxPageModule,
+    MtxAppBarModule,
+    MtxContainerModule,
+    MtxTextModule,
+    MtxToolbarModule,
+    MatDividerModule,
+    FooterSectionModule
   ]
 })
 export class MtxColorDemoModule { }
