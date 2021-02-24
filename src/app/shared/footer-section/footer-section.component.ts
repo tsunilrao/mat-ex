6d690336c44db103b35da919df65e8048772260d
tsunilrao <t_sunil_rao@yahoo.com>
2021-02-25 03:55:25 +0530

    Home UI optimization

--- src/app/shared/footer-section/footer-section.component.ts
+++ src/app/shared/footer-section/footer-section.component.ts
@@ -5,7 +5,7 @@ import { version } from 'projects/mat-ex/package.json';
   selector: 'app-footer-section',
   templateUrl: './footer-section.component.html',
   styleUrls: ['./footer-section.component.scss'],
-  host: { class: 'mtx-primary center mtx-page-full-width mat-elevation-z4' }
+  host: { class: 'mtx-primary center mat-elevation-z4' }
 })
 export class FooterSectionComponent implements OnInit {
 
