821623718b2ddcd14b0bef7876f7ace8984098b7
tsunilrao <t_sunil_rao@yahoo.com>
2021-02-26 13:17:01 +0530

    Scrollview ripple added

--- src/app/home/home-scrollview-card/home-scrollview-card.module.ts
+++ src/app/home/home-scrollview-card/home-scrollview-card.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeScrollviewCardComponent } from './home-scrollview-card.component';
 import { MtxCardModule, MtxTextModule, MtxContainerModule, MtxToolbarModule } from 'projects/mat-ex/src/public-api';
+import { MatRippleModule } from '@angular/material/core';
 
 
 
@@ -9,7 +10,8 @@ import { MtxCardModule, MtxTextModule, MtxContainerModule, MtxToolbarModule } fr
   declarations: [HomeScrollviewCardComponent],
   imports: [
     CommonModule,
-    MtxCardModule, MtxTextModule, MtxContainerModule, MtxToolbarModule
+    MtxCardModule, MtxTextModule, MtxContainerModule, MtxToolbarModule,
+    MatRippleModule
   ],
   exports: [HomeScrollviewCardComponent]
 })
